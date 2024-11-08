39499640011776e1f2af71e3ebd86d50478238ae
MelinaGrgt <georgetm@hotmail.fr>
2024-11-08 14:42:46 +0100

    feat: initial commit

--- app/Views/admin/item/genre.php
+++ app/Views/admin/item/genre.php
@@ -9,7 +9,7 @@
 </div>
 <div class="row">
     <div class="col-md-4">
-        <form action="/admin/item/creategenre" method="POST">
+        <form action="<?=base_url('/admin/item/creategenre')?>" method="POST">
             <div class="card">
                 <div class="card-header">
                     <h5>Ajouter un genre</h5>
